760a2e2bd1aef97df95e1557dd0526902fa917db
Bob Colts <rcolts34@gmail.com>
2024-10-01 19:12:58 -0400

    After page 34
    Cloud Cover Started

--- Mission Demolition/Assets/FollowCam.cs
+++ Mission Demolition/Assets/FollowCam.cs
@@ -8,6 +8,7 @@ public class FollowCam : MonoBehaviour
 
     [Header("Inscribed")]
     public float easing = 0.05f;
+    public Vector2 minXY = Vector2.zero; // Vector2.zero is [0,0]
 
     [Header("Dynamic")]
     public float camZ; // The desired Z pos of the camera
@@ -18,12 +19,34 @@ public class FollowCam : MonoBehaviour
     }
     void FixedUpdate()
     {
-        if (POI == null) return;
-        Vector3 destination = POI.transform.position;
+
+        // if (POI == null) return;
+        // Vector3 destination = POI.transform.position;
+        Vector3 destination = Vector3.zero;
+        if (POI != null)
+        {
+            // if the POI has a rigidbody, check to see if it's sleeping
+            Rigidbody poiRigid = POI.GetComponent<Rigidbody>();
+            if ((poiRigid != null) && poiRigid.IsSleeping())
+            {
+                POI = null; 
+            }
+        }
+
+        if (POI != null)
+        {
+            destination = POI.transform.position;
+        }
+
+        // Limit the minimum values of destination.x & destination.y
+        destination.x = Mathf.Max(minXY.x, destination.x);
+        destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
         destination.z = camZ;
-        // set the camera's position
+        // Set the camera's position
         transform.position = destination;
+        // Set the orthographicSize of the Camera to keep Ground in view
+        Camera.main.orthographicSize = destination.y + 10;
     }
 
 }
